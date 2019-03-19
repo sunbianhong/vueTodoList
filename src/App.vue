af49ec30fc9b3760eec42393fea92be3875d79bc
shb <1531068831@qq.com>
2019-03-19 10:59:25 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,42 @@
 <template>
   <div id="app">
-    <router-link to="/normal">show normalTodoList</router-link><br/>
-    <router-link to="/eventbus">show eventBusTodoList</router-link><br/>
+    <router-link to="/normal">show normalTodoList</router-link>
+    <br>
+    <router-link to="/eventbus">show eventBusTodoList</router-link>
+    <br>
     <!-- <router-link to="/vuextodo">show vuexTodoList</router-link> -->
     <p>test1</p>
+    <p>test1-1</p>
     <div class="childBox">
-        <router-view/>
+      <router-view/>
     </div>
   </div>
 </template>
 
-<script>  
+<script>
 export default {
-  name: 'App',
+  name: "App",
   data() {
-    return { }
+    return {};
   },
   methods: {
-    showAdvice: function (advice) {
-      alert(advice)
+    showAdvice: function(advice) {
+      alert(advice);
     }
-  },
-}
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: left;
   color: #2c3e50;
   margin-top: 10px;
 }
-.childBox{
+.childBox {
   padding: 10px;
   background-color: #ffa;
   height: 300px;
